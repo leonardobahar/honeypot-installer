509e028926c4f86b954c0e6f370440f91e93c28b
Leonardo Theoson Bahar <33883361+leonardobahar@users.noreply.github.com>
2025-04-05 11:00:25 +0700

    chore(honeytrap): alert log location

--- install_honeytrap.sh
+++ install_honeytrap.sh
@@ -3,3 +3,6 @@ docker pull honeytrap/honeytrap:latest
 docker run -d -p 22:8022 -p 23:8023 -p 5900:5900 -p 8080:8080 --name honeytrap honeytrap/honeytrap:latest
 docker cp ./honeytrap_config.toml honeytrap:/config/config.toml
 docker restart honeytrap
+echo "===================="
+echo "Logs are available on /var/lib/docker/containers/<CONTAINER_NAME>/<CONTAINER_NAME.log>"
+echo "===================="
